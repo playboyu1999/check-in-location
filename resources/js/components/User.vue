737b016ebac60ea625ba252a30b537e93100d7c5
pungpondstr <50855616+pungpondstr@users.noreply.github.com>
2019-08-21 14:54:41 +0700

    commit

---
+++ resources/js/components/User.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <table class="table table-bordered">
+            <tr align="center">
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+
+            <tr align="center" v-for="user in users">
+                <th>{{user.id}}</th>
+                <td>{{user.name}}</td>
+                <td>{{user.email}}</td>
+            </tr>
+
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.show_data()
+        },
+        methods:{
+            show_data(){
+                axios.get('api/users').then(response=>{
+                    this.users = response.data;
+                })
+            }
+        },
+        data(){
+            return{
+                users:[],
+                user:{
+                    id:0,
+                    name:'',
+                    email:''
+                }
+            }
+        }
+    }
+</script>
